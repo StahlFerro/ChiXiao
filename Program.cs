cc4cb93880eb583cda75e71867a94d1450f24231
StahlFerro <StahlFerro@users.noreply.github.com>
2021-03-22 13:10:35 +0700

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChiXiao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
